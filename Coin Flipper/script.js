9b6984b06060fcafbc8db85d5f7c045bc99249fe
KareemHassan <kareem99710@gmail.com>
2025-02-05 22:15:36 +0200

    add comments to the js

--- Coin Flipper/script.js
+++ Coin Flipper/script.js
@@ -1,14 +1,16 @@
-const body = document.getElementsByTagName("body")[0];
+const body = document.getElementsByTagName("body")[0]; //[0] to call the first element of the array which is the only one (body)
 
 function setColor(color){
-    body.style.backgroundColor = color;
+    body.style.backgroundColor = color; //set the background color to the parameter color sent from the html file
 }
 
 function randomColor(){
+    //let the RGB get random numbers each (0-255 , 0-255 , 0-255) and round them to prevent getting floats;
     const R = Math.round(Math.random()*255);
     const G = Math.round(Math.random()*255);
     const B = Math.round(Math.random()*255);
 
+    //change the rgb from numbers to string by using String interpolation
     const color = `rgb(${R} ${G} ${B})`;
     body.style.backgroundColor = color;
 }
