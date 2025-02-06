54614680cd7f830e443c7f590d09dd2deb094964
KareemHassan <kareem99710@gmail.com>
2025-02-06 17:47:38 +0200

    Add comments

--- Palindrome checker/script.js
+++ Palindrome checker/script.js
@@ -1,11 +1,12 @@
-const input = document.getElementById("input");
-
+const input = document.getElementById("input"); //letting the variable be equal to the what we got from user input
 function check(){
-    const value = input.value;
-    const statue = reverse(value);
+    const value = input.value; //letting a variable be equal to the string we got from the user
+    const statue = reverse(value); //letting the variable be equal to the returned bool from the checking function  (we sent the string as the parameter which is to be checked)
+    //if the variable is true
     if(statue){
         alert("The word you entered is a palindrome");
     }
+    //if its false
     else{
         alert("The word you entered isn't a palindrome");
     }
@@ -15,6 +16,7 @@ function reverse(value){
     let j = 0;
     let isrev = true;
     
+    //loop to check if the end is equal to the beginning 
     for(let i = value.length - 1; i >= 0; i--){
         if(value[i] !== value[j]){
             isrev = false; 
