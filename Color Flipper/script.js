f7d376c463133f7dc8d2c83195463cfc2d4ca388
KareemHassan <kareem99710@gmail.com>
2025-03-04 20:06:04 +0200

    display RGB

--- Color Flipper/script.js
+++ Color Flipper/script.js
@@ -1,12 +1,15 @@
 const body = document.getElementsByTagName("body")[0]; //[0] to call the first element of the array which is the only one (body)
-
+const dispRGB = document.getElementById("colorRGP");
 function setColor(color){
-    body.style.backgroundColor = color; //set the background color to the parameter color sent from the html file
+    body.style.backgroundColor = color; //set the background color to the parameter color sent from the html file 
+    const ColorRGB = getComputedStyle(body).backgroundColor; // Get the RGB of the color
+    dispRGB.textContent = ColorRGB; // Display the RGB value
 }
 
 const bodycolor = getComputedStyle(body).backgroundColor; // a constant variable that gets the background color of the body and stores it
 function removeColor(){
     body.style.backgroundColor = bodycolor; //convert the body background back to original when remove color button is clicked
+    dispRGB.textContent = "rgb(44, 44, 44)";
 }
 
 function randomColor(){
@@ -18,4 +21,5 @@ function randomColor(){
     //change the rgb from numbers to string by using String interpolation
     const color = `rgb(${R} ${G} ${B})`;
     body.style.backgroundColor = color;
+    dispRGB.textContent = `RGB (${R},${G},${B})`;
 }
