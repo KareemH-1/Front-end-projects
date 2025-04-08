86727f567c5969a89e96fac4435949196ae05c64
KareemHassan <kareem99710@gmail.com>
2025-04-08 07:49:52 +0200

    fix: clear result message after displaying feedback in Number Guessing Game

--- Number Guessing Game/script.js
+++ Number Guessing Game/script.js
@@ -15,18 +15,26 @@ submit.addEventListener('click', () => {
     if (guess === randomNum) {
         result.innerHTML = "Congratulations you found the number";
         result.style.color = "green";
+        setTimeout(() => {
+            result.innerHTML = "";
+        }, 2000);
         level *= 10;
         randomNum = findRandomNum();
         changeInstruction();
     } else if (guess < randomNum) {
         result.innerHTML = "Too low";
         result.style.color = "orange";
+        setTimeout(() => {
+            result.innerHTML = "";
+        }, 2000);
     } else {
         result.innerHTML = "Too high";
         result.style.color = "red";
+        setTimeout(() => {
+            result.innerHTML = "";
+        }, 2000);
     }
 });
-
 function changeInstruction() {
     instruction.innerHTML = "Guess a number between 1 and " + (10 * level);
 }
